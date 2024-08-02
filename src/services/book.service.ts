55a1a13e39fbead63168791ca062a2e62d92c198
Patranto Bagus Prabowo <patranto.prabowo@gmail.com>
2024-08-02 22:11:14 +0700

    fixing book stock

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import Book from 'src/entities/book.entity';
+import BorrowBook from 'src/entities/borrow_book.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,6 +9,9 @@ export class BookService {
     constructor(
         @InjectRepository(Book)
         private bookRepo: Repository<Book>,
+
+        @InjectRepository(BorrowBook)
+        private borrowBookRepo: Repository<BorrowBook>,
     ) { }
 
     async create(bookData: Partial<Book>): Promise<Book> {
@@ -17,7 +21,7 @@ export class BookService {
 
     async checkBooks(): Promise<Book[]> {
         const books = await this.bookRepo.find();
-        const borrowedBooks = await this.bookRepo.find({ relations: ['books'] });
+        const borrowedBooks = await this.borrowBookRepo.find({ relations: ['book'] });
 
         return books.map(book => ({
             ...book,
